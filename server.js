559b49ad3dd101254bbc2f051e9b9a88aa619da1
pelkap <peterchongo@hotmail.com>
2016-10-27 15:16:40 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,13 +54,15 @@ var htmlTemplate = `
    </body>
 </html>
 `;
+return htmlTemplate;
+    
 }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
