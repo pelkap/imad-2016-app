5b17cc9e342b73134ddc770c30f537a4da894142
pelkap <peterchongo@hotmail.com>
2016-10-27 15:31:55 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ function createTemplate (data) {
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
-    var content = data.contgent;
+    var content = data.content;
 
 var htmlTemplate = `
 <html>
